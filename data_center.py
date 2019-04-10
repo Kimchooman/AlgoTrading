32940eba8a1fa26ae155ddf2715b7866b1d454f0
Kimchooman <43964171+Kimchooman@users.noreply.github.com>
2019-04-09 20:51:13 -0400

    Update data_center.py

--- data_center.py
+++ data_center.py
@@ -1,1 +1,23 @@
+import requests
+import json
 
+class data_center:
+
+	def __init__(self,ticker): #Defining json url, and using %s for dynamic inputs.
+		self.ticker = ticker
+		self.url = 'https://api.iextrading.com/1.0/stock/%s/chart/5y'
+		self.data = self.download_data()
+
+	def download_data(self): #Getting Raw data, subclasses included.
+		my_url = self.url %(self.ticker)
+		response = requests.get(my_url)
+		data = json.loads(response.text)
+
+		return data
+
+	def view_data(self, key): #Viewing individual data sets.
+		rtn = []
+		for daily in self.data:
+			rtn.append(daily[key])
+
+		return rtn
