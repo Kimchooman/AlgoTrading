053f63ae3cef294da2f6deff2feeb7e0de9e5b60
Kimchooman <43964171+Kimchooman@users.noreply.github.com>
2019-04-09 22:07:21 -0400

    Update algo_center.py

--- algo_center.py
+++ algo_center.py
@@ -1,28 +1,19 @@
 import talib
 
+#Durig and after the development of new strategies,
+#this class will be a full object that contains
+#funcions that return buy and sell requests based on 
+#the given time-series data
+
 class algo_center:
+	
 	def __init__(self):
 		_ = None
 			
 	def bare_mr(self,data):
-
 		rsi = talib.RSI(data, timeperiod=14)
 
 		if rsi[-1] >= 70:
 			return False
-
 		elif rsi[-1] <= 30:
 			return True
-
-	def stationary_ar(self,data):
-
-		n = 0
-		rtn = []
-		for point in data:
-
-			if point == data[0]:
-				pass
-			else:
-				rtn.append(point-data[n]) 
-				n += 1
-		return rtn
