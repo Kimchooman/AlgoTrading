7d054d913ff3767e44a796b28194ce452774c70f
Kimchooman <43964171+Kimchooman@users.noreply.github.com>
2019-04-09 20:52:35 -0400

    Create simulated_portfolio.py

---
+++ simulated_portfolio.py
@@ -0,0 +1,26 @@
+class simulated_portfolio:
+
+	def __init__(self):  #Defining object attributes.
+		self.capital = 10000
+		self.comission = 10
+		self.current_hold = None
+		self.pos_size = 0
+		self.rtn = 0
+
+	def buy(self, current_price):  #Calculating if suffient funds exist, then calculating position size
+		if current_price < self.capital:
+			self.pos_size = self.capital // current_price
+			self.current_hold = current_price
+
+		else:
+			print("insuffient funds")
+
+	def sell(self, sell_price): #Calculate return, then reseting positions to zero.
+		self.rtn += ((self.current_hold - sell_price) * self.pos_size)
+		self.current_hold = None
+		self.pos_size = 0
+
+	def calculated_returns(self): #Factoring in commision to final returns.
+		net_returns = (self.rtn - self.comission)
+
+		return net_returns
